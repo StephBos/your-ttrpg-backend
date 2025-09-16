33090eeb418a1d3746a0c5a73a56b3ee7e6dc64b
Stephen Bos <stephen.bos1029@gmail.com>
2025-09-16 15:31:24 -0600

    Create route working

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,4 +1,5 @@
 import { query } from '../config/database.js'
+import argon2 from 'argon2'
 
 interface UserRow {
     userName: string;
@@ -27,15 +28,30 @@ async function checkUsername(username: string | undefined): Promise<boolean> {
     }
 }
 
+async function createUser(username: string, email: string, password: string){
+    console.info('Creating user with username: ', username, ' and email ', email)
+    const hashedPw = await argon2.hash(password)
+
+    try {
+        const result = await query('INSERT into users (username, email, password) ' +
+                    'VALUES ($1, $2, $3) ' +
+                    'returning username, email, created_at',
+                    [username, email, hashedPw]
+        )
+        return result.rows[0]
+    } catch(error) {
+        console.error('Error creating user:', username, ' error: ', error)
+        throw error
+    }
+}
+
 function convertToArray(resultObjs: UserRow[]): string[] {
     const userNames: string[] = resultObjs.map((u: UserRow) => u.userName)
     console.info("Returning: ", userNames)
     return userNames
 }
 
-function createUser(username: string, email: string, password: string){
-    console.log('creating user.')
-}
+
 
 export { getAllUserNames, checkUsername, createUser }
 
