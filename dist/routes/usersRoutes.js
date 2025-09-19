f7bfa0d9d20dc29451b2983d06c4b7dd55e7b13f
Stephen Bos <stephen.bos1029@gmail.com>
2025-09-19 14:56:48 -0600

    Login verification

--- dist/routes/usersRoutes.js
+++ dist/routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUserNames, checkUsername, createUser, checkEmail } from '../controllers/usersControllers.js';
+import { getAllUserNames, checkUsername, createUser, checkEmail, verifyLogin } from '../controllers/users/usersControllers.js';
 const router = express.Router();
 router.get('/', async (req, res) => {
     console.info('Getting all usernames');
@@ -10,6 +10,15 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+router.get('/login/:username', async (req, res) => {
+    console.info('Verifying login');
+    try {
+        res.json(await verifyLogin(req.body.username, req.body.password));
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Error logging in' });
+    }
+});
 router.get('/:username', async (req, res) => {
     console.info('Checking username: ', req.params.username);
     try {
@@ -18,7 +27,7 @@ router.get('/:username', async (req, res) => {
     }
     catch (error) {
         console.error('Error in checkUsername route:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Error checking username' });
     }
 });
 router.post('/', async (req, res) => {
@@ -31,15 +40,14 @@ router.post('/', async (req, res) => {
         }
         const emailInUse = await checkEmail(email);
         if (emailInUse) {
-            console.log('gonna send a 409');
-            return res.status(409).json({ success: false, error: "Email already in use" });
+            return res.status(409).json({ success: false, error: 'Email already in use' });
         }
         const newUser = await createUser(username, email, password);
         return res.status(201).json(newUser);
     }
     catch (error) {
         console.error("Error creating user:", error);
-        return res.status(500).json({ error: "Internal server error" });
+        return res.status(500).json({ error: "Error creating user" });
     }
 });
 export default router;
